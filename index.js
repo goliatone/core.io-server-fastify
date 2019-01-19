3ec3c1269e915e124a02e4606fd412e029d529ae
goliatone <burgosemi@gmail.com>
2019-01-18 21:47:42 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*jshint esversion:6, node:true*/
+'use strict';
+
+/*
+ * Default initializer for the module.
+ *
+ * You can always override this and make
+ * a custom initializer.
+ */
+// module.exports.init = require('./lib/init');
+
+
+module.exports.initializeSubapp = require('./lib/initializeSubapp');
